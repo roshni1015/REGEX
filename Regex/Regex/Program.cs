b1b1c988b6e88b5b94878d97de2eb03e169f7242
roshni1015 <roshniadatrao1110@gmail.com>
2022-05-18 15:05:17 +0530

    [Roshni]-FirstName

--- Regex/Regex/Program.cs
+++ Regex/Regex/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using REGEX;
+
 Console.WriteLine("Welcome to User Registration!");
+UserRegistration userRegistration = new UserRegistration();
+Console.WriteLine("Enter First Name");
+string Firstname = Console.ReadLine();
+userRegistration.FirstName(Firstname);
---
+++ Regex/Regex/UserRegistration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace REGEX
+{
+    public class UserRegistration
+    {
+        public bool FirstName(string Firstname)
+        {
+            bool status;
+            string firstname = "^[A-Z]{1}[a-z]{2,}$";
+            Regex regex = new Regex(firstname);
+
+            if (regex.IsMatch(Firstname))
+            {
+                status = true;
+                Console.WriteLine("First Name is Valid ");
+            }
+            else
+            {
+                status = false;
+                Console.WriteLine("Please Enter First Letter Capital");
+            }
+            return status;
+
+        }
+
+    }
+}
