9408e6071273f9a5430c4c294e3752d2e7efa3fc
roshni1015 <99880059+roshni1015@users.noreply.github.com>
2022-05-18 18:59:44 +0530

    Update Program.cs

--- Regex/Regex/Program.cs
+++ Regex/Regex/Program.cs
@@ -41,7 +41,7 @@ while (true)
             Console.WriteLine("******************************************************************");
             break;
         default:
-            Console.WriteLine("Worng Input.");
+            Console.WriteLine("Wrong Input.");
             break;
     }
 }
